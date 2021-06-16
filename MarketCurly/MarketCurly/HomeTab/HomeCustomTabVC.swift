482b0aed88d96fba951989f65245ebccb0f3a8f1
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-06-17 01:59:45 +0900

    refactor : 커스텀탭바 코드 리팩토링

--- MarketCurly/MarketCurly/HomeTab/HomeCustomTabVC.swift
+++ MarketCurly/MarketCurly/HomeTab/HomeCustomTabVC.swift
@@ -13,15 +13,17 @@ class HomeCustomTabVC: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var indicatorBar: UIView!
     @IBOutlet weak var stackviewWidth: NSLayoutConstraint!
+    @IBOutlet weak var indicatorBarWidth: NSLayoutConstraint!
     
     let titleList : [String] = ["컬리추천", "신상품", "베스트", "알뜰쇼핑", "금주혜택"]
     var nowPage : Int = 0
     let viewSizeWidth : CGFloat = UIScreen.main.bounds.width
     let viewSizeHeight : CGFloat = UIScreen.main.bounds.height
+    let indicatorSize : CGFloat = UIScreen.main.bounds.width * (69/375)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        indicatorBarWidth.constant = viewSizeWidth * (69/375)
         stackviewWidth.constant = viewSizeWidth
         registerXib()
         scrollView.delegate = self
@@ -36,64 +38,50 @@ class HomeCustomTabVC: UIViewController {
         collectionView.register(nib, forCellWithReuseIdentifier: HomeTitleLabelCVC.identifier)
     }
     
-    func setScrollTitleColor() {
-        if scrollView.contentOffset == CGPoint(x: 0.0, y: 0.0) {
-            print("0,0 되나?")
-            nowPage = 0
-            UIView.animate(withDuration: 0.1){
-                //self.indicatorBar.frame.origin.x = 16
-                self.indicatorBar.frame.origin.x = self.viewSizeWidth * (15/375)
-            }
-            
-            
-        }
-        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth, y: 0.0){
-            print("390 나옴?") // 나온당 ㅎㅎ
-            nowPage = 1
-            UIView.animate(withDuration: 0.1){
-                self.indicatorBar.frame.origin.x = 8 + self.viewSizeWidth/5
-            }
-            
-        }
-        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth * 2, y: 0.0){
-            print("3번째 뷰컨")
-            nowPage = 2
-            UIView.animate(withDuration: 0.1){
-                self.indicatorBar.frame.origin.x = 4 + self.viewSizeWidth/5 * 2
-            }
-        }
-        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth * 3, y: 0.0){
-            print("4번째 뷰컨")
-            nowPage = 3
-            UIView.animate(withDuration: 0.1){
-                self.indicatorBar.frame.origin.x = self.viewSizeWidth/5 * 3
-            }
-        }
-        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth * 4, y: 0.0){
-            print("5번째 뷰컨")
-            nowPage = 4
-            print(nowPage)
-            UIView.animate(withDuration: 0.1){
-                //self.indicatorBar.center = CGPoint(x: (390.0/5)*5, y: 130)
-                self.indicatorBar.frame.origin.x = self.viewSizeWidth/5 * 4 - 4
-            }
-            
-        }
-        
-        
-        
-        
-    }
+    
 }
 
 extension HomeCustomTabVC : UIScrollViewDelegate {
     
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        setScrollTitleColor()
-        print("스크롤뷰")
+        
+        if scrollView.contentOffset == CGPoint(x: 0.0, y: 0.0) {
+            nowPage = 0
+            UIView.animate(withDuration: 0.1){
+                self.indicatorBar.frame.origin.x = self.viewSizeWidth * (15/375)
+            }
+        }
+        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth, y: 0.0){
+            nowPage = 1
+            
+            UIView.animate(withDuration: 0.1){
+                self.indicatorBar.frame.origin.x = self.viewSizeWidth*(15/375) + self.indicatorSize
+            }
+             
+        }
+        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth * 2, y: 0.0){
+            nowPage = 2
+            UIView.animate(withDuration: 0.1){
+                self.indicatorBar.frame.origin.x = self.viewSizeWidth*(15/375) + self.indicatorSize * 2
+            }
+        }
+        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth * 3, y: 0.0){
+            nowPage = 3
+            UIView.animate(withDuration: 0.1){
+                self.indicatorBar.frame.origin.x = self.viewSizeWidth*(15/375) + self.indicatorSize * 3
+            }
+        }
+        else if scrollView.contentOffset == CGPoint(x: viewSizeWidth * 4, y: 0.0){
+            nowPage = 4
+            print(nowPage)
+            UIView.animate(withDuration: 0.1){
+                self.indicatorBar.frame.origin.x = self.viewSizeWidth*(15/375) + self.indicatorSize * 4
+            }
+            
+        }
         
         collectionView.reloadData()
-        
     }
  
     
@@ -125,10 +113,6 @@ extension HomeCustomTabVC : UICollectionViewDelegate {
             break
         }
         
-
-        print("컬렉션뷰")
-        collectionView.reloadData()
-        print("컬렉션뷰2")
     }
 }
 
